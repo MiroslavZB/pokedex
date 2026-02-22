295acf396cfef5fba6c06181a9e2cb9689a4920b
MiroslavZB <miroslav.z.blagoev@gmail.com>
2026-02-22 20:17:17 +0000

    made colors used be the same for the same type

--- lib/modules/weather/components/current_weather_summary_card.dart
+++ lib/modules/weather/components/current_weather_summary_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/modules/pokemon_details/models/pokemon_type.dart';
 import 'package:pokedex/shared/functions.dart';
 
 class CurrentWeatherSummaryCard extends StatelessWidget {
@@ -9,13 +10,16 @@ class CurrentWeatherSummaryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Create a temporary PokemonTypeInfo to get the color
+    final typeInfo = PokemonTypeInfo(name: suggestedType, url: '');
+    
     return Row(
       children: [
         Container(
           margin: EdgeInsets.only(right: 20),
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           decoration: BoxDecoration(
-            color: _getTypeColor(suggestedType),
+            color: typeInfo.displayColor,
             borderRadius: BorderRadius.circular(20),
           ),
           child: Text(
@@ -36,29 +40,4 @@ class CurrentWeatherSummaryCard extends StatelessWidget {
       ],
     );
   }
-}
-
-Color _getTypeColor(String typeName) {
-  final typeColors = {
-    'normal': const Color(0xFFA8A878),
-    'fire': const Color(0xFFF08030),
-    'water': const Color(0xFF6890F0),
-    'electric': const Color(0xFFF8D030),
-    'grass': const Color(0xFF78C850),
-    'ice': const Color(0xFF98D8D8),
-    'fighting': const Color(0xFFC03028),
-    'poison': const Color(0xFFA040A0),
-    'ground': const Color(0xFFE0C068),
-    'flying': const Color(0xFFA890F0),
-    'psychic': const Color(0xFFF85888),
-    'bug': const Color(0xFFA8B820),
-    'rock': const Color(0xFFB8A038),
-    'ghost': const Color(0xFF705898),
-    'dragon': const Color(0xFF7038F8),
-    'dark': const Color(0xFF705848),
-    'steel': const Color(0xFFB8B8D0),
-    'fairy': const Color(0xFFEE99AC),
-  };
-
-  return typeColors[typeName.toLowerCase()] ?? Colors.grey;
 }
